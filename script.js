a572eca23a182b1f8beab545001c94780595cbf6
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-02-17 19:48:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,10 +10,9 @@ let result = Math.ceil(Math.random() * 20);
 let result1 = Math.floor(Math.random() * -20 - 1);
 
 btn.addEventListener(`click`, function () {
-	num.textContent = btn.nodeValue;
   if (input.value >= result1 && input.value <= result) {
-    response.textContent = `Hot`;
+    num.textContent = `Hot`;
   } else {
-    response.textContent = `Cold`;
+    rnum.textContent = `Cold`;
   }
 });
