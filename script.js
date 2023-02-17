865555ed40d08ed25919ec98ad039c7369995572
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-02-17 19:32:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 const input = document.querySelector(`#guess`);
 
 const btn = document.querySelector(`#btn`);
-
+const num = document.querySelector(`#num`);
 const response = document.querySelector(`#respond`);
 
 let result = Math.ceil(Math.random() * 20);
 let result1 = Math.floor(Math.random() * -20 - 1);
 
 btn.addEventListener(`click`, function () {
+	num.textContent = btn.nodeValue;
   if (input.value >= result1 && input.value <= result) {
     response.textContent = `Hot`;
   } else {
